685cea3601b35644193210a2a36b356bf7e530a9
Ritheesh <ritheesh.4@gmail.com>
2020-05-04 15:26:33 +0530

    division input added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const users = require("./users.json");
+const division = require("./division.json")
 
 const addStudent = (name, id) => {
     let user = {
@@ -11,7 +12,7 @@ const addStudent = (name, id) => {
 
     fs.writeFile("users.json", JSON.stringify(users), err => {
         if (err) throw err;
-        console.log("Done writing"); 
+        console.log("Done writing");
         console.log(users)
     });
 }
@@ -23,14 +24,54 @@ const marks = (id, subject, mark) => {
         mark: mark,
 
     };
-  
-    users[index].marks = marksDetails;
+
+    users[index].marks.push(marksDetails);
     fs.writeFile("users.json", JSON.stringify(users), err => {
         if (err) throw err;
-        console.log("Done writing");    
+        console.log("Done writing");
     });
-
-
 }
 
-marks(6, 'malayalam', 60)
+const editMarks = (id, subject, marks) => {
+    let index = users.findIndex(obj => obj.id == id);
+    console.log("index", index)
+    let allObjects = users[index];
+    console.log("all objects", allObjects)
+    let correspondingObject = allObjects.marks;
+    console.log("corresopondign objects", correspondingObject);
+
+
+    for (let i = 0; i < correspondingObject.length; i++) {
+        // console.log(allObjects[i].subject)
+        if (correspondingObject[i].subject === subject) {
+            correspondingObject[i].mark = marks;
+
+        }
+
+    }
+
+
+    fs.writeFile("users.json", JSON.stringify(users), err => {
+        if (err) throw err;
+        console.log("Done writing");
+    });
+}
+
+
+
+const addDivision = (name, teacherName, students) => {
+    let divisionInput = {
+        name: name,
+        teacherName: teacherName,
+        students: students
+    };
+    division.push(divisionInput);
+
+    fs.writeFile("division.json", JSON.stringify(division), err => {
+        if (err) throw err;
+        console.log("Done writing");
+        console.log(division)
+    });
+}
+
+addDivision("Class A", "Renjisha e rajan", ["ritheesh", "renjith", "anandhu", "rupesh"]);
